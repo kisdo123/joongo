31c86c9c8793e87fba2b7fbaae5e8e14fafbe49a
김승용 <nnvkk@naver.com>
2018-11-28 19:49:30 +0900

    메인

--- joongo/src/main/webapp/script/main.js
+++ joongo/src/main/webapp/script/main.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+$(function(){
+	
+	// 마지막 line 클래스 없애기
+	$('.line').last().remove();
+	
+	// 카테고리 하나를 기본으로 잡아놓음
+	$('.category-button').first().css({
+		"border-color": "#f64448",
+		"background-color": "#f64448",
+		"color": "white"
+	});
+	
+	// 카테고리 버튼을 눌렀을 시 css 변경
+	$('.category-button').click(function(){
+		$(".category-button").each(function(){
+			$(this).css({
+				"border-color": "#e8e8e8",
+				"background-color": "white",
+				"color": "black"
+			});
+		});
+		$(this).css({
+			"border-color": "#f64448",
+			"background-color": "#f64448",
+			"color": "white"
+		});
+	})
+})
